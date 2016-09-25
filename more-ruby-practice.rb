37b9d7d54418a639d613e007982ffc2bd395f16b
Jonah Osawa <jonahosawa@MacBook-Pro-2.local>
2016-09-25 11:57:54 -0400

    this is my first commit

---
+++ more-ruby-practice.rb
@@ -0,0 +1,12 @@
+
+
+puts ("What is your most favorite thing?")
+thing_one = gets.strip
+
+puts ("What is your second most favorite thing?")
+thing_two = gets.strip
+
+puts ("What is your third most favorite thing?")
+thing_three = gets.strip
+
+puts ("These are a few of my favorite things: #{thing_one}, #{thing_two}, #{thing_three}")
